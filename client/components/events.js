7af5aa0e9cf4ad4fa3b1f33b34f6e8a55e9d94aa
hanz <lhz0516@gmail.com>
2016-09-19 00:47:39 -0700

    event detail on event  list

--- client/components/events.js
+++ client/components/events.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Link} from 'react-router';
-
+import moment from 'moment';
 import Loading from 'client/components/common/loading';
 
 export default (props) => {
   const T = props.context.T;
   return (
-    <div className="container">
+    <div className="container" style={{ paddingTop: '60px'}}>
       <div className="row">
         {
           props.events.status === 'ready' ?
@@ -14,18 +14,18 @@ export default (props) => {
               props.events.data.map((event, index) =>
                 <div key={index} className="col-sm-12">
                   <div className="hpanel blog-box">
-                    <div className="panel-heading">
-                      <div className="media clearfix">
-                        <Link to="#" className="pull-left">
-                          <img src="/images/a8.jpg" alt="profile-picture" />
-                        </Link>
-                        <div className="media-body">
-                          <small>Created by: <span className="font-bold">Mike Smith</span></small>
-                          <br />
-                            <small className="text-muted">21.03.2015, 06:45 pm</small>
-                        </div>
-                      </div>
-                    </div>
+                    {/*<div className="panel-heading">*/}
+                      {/*<div className="media clearfix">*/}
+                        {/*<Link to="#" className="pull-left">*/}
+                          {/*<img src="/images/a8.jpg" alt="profile-picture" />*/}
+                        {/*</Link>*/}
+                        {/*<div className="media-body">*/}
+                          {/*<small>Created by: <span className="font-bold">Mike Smith</span></small>*/}
+                          {/*<br />*/}
+                            {/*<small className="text-muted">21.03.2015, 06:45 pm</small>*/}
+                        {/*</div>*/}
+                      {/*</div>*/}
+                    {/*</div>*/}
                     <div className="panel-body">
                       <Link to={`/events/${event._id}`}>
                         <h4>{event.title}</h4>
@@ -34,9 +34,9 @@ export default (props) => {
                     </div>
                     <div className="panel-footer">
                     <span className="pull-right">
-                        <i className="fa fa-comments-o" /> 22 <T.text text={{ key: 'comments' }} />
+                      Fee: {event.unit === 'dollar' ? '$' : '¥'} {event.fee}
                     </span>
-                      <i className="fa fa-eye" /> 142 views
+                      {moment(event.time).format('MM-DD-YYYY')} | {event.location}
                     </div>
                   </div>
                 </div>
