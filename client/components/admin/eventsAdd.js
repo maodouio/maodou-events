c908a469c437b4728d57c83684a6a35efa19ed8f
hanz <lhz0516@gmail.com>
2016-09-20 21:19:47 -0700

    translate to Chinese

--- client/components/admin/eventsAdd.js
+++ client/components/admin/eventsAdd.js
@@ -17,37 +17,37 @@ export default class EventAdd extends React.Component {
     return (
       <div className="row">
         <div className="col-sm-12">
-          <h1>New Events</h1>
+          <h1>新建活动</h1>
           <form onSubmit={this.props.addEvent}>
             <div className="form-group">
-              <label>Title</label>
+              <label>活动名称</label>
               <input className="form-control" type="text" placeholder="title" name="title" />
             </div>
             <div className="form-group">
-              <label>Date</label>
+              <label>日期</label>
               <input className="form-control" type="date" placeholder="time" name="time" />
             </div>
             <div className="form-group">
-              <label>Location</label>
+              <label>地点</label>
               <input className="form-control" type="text" placeholder="location" name="location" />
             </div>
             <div className="form-group">
-              <label>Limit</label>
+              <label>人数限制</label>
               <input className="form-control" type="text" placeholder="limit" name="limit" />
             </div>
             <div className="row">
               <div className="col-xs-5">
                 <div className="form-group">
-                  <label>Unit</label>
+                  <label>付款单位</label>
                   <select defaultValue="dollar" onChange={this.changeUnit} className="form-control" name="unit">
-                    <option value="dollar">Dollar</option>
+                    <option value="dollar">美金</option>
                     <option value="rmb">RMB</option>
                   </select>
                 </div>
               </div>
               <div className="col-xs-7">
                 <div className="form-group">
-                  <label>Fee</label>
+                  <label>费用</label>
                   <div className="input-group m-b">
                     <span className="input-group-addon">{this.state.unit}</span>
                     <input type="text" name="fee" className="form-control"/>
@@ -56,7 +56,7 @@ export default class EventAdd extends React.Component {
               </div>
             </div>
             <br />
-            <label>Description</label>
+            <label>活动描述</label>
             <div id="editor" />
             <button className="btn btn-default" type="submit">Submit</button>
           </form>
